c30edd4197dedcd3894f33f2ef167bb543f91ceb
Cem GEÇGEL <aa-albara@hotmail.com>
2020-11-20 09:56:18 +0300

    belirteç oluşturuldu

--- Yasayıcı.h
+++ Yasayıcı.h
@@ -1,6 +1,28 @@
 #ifndef YASIYICI
 #define YASAYICI
 
+#include <string>
 
+namespace bad
+{
+	typedef std::string dize;
+
+	struct belirteç_türü
+	{
+		dize ad;
+		dize desen;
+	};
+
+	struct belirteç
+	{
+		belirteç_türü tür;
+		dize beden;
+	};
+
+	const belirteç_türü türler[1] =
+	{
+		{ "tanýmlayýcý", "^[a-zA-Z_][a-zA-Z_0-9]*" }
+	};
+}
 
 #endif
